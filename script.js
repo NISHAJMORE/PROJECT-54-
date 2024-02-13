882eb7f21eb08c23c50e95f382e08cd3e352885e
NISHAJMORE <120033928+NISHAJMORE@users.noreply.github.com>
2024-02-13 15:49:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function calculate()
+{
+   length=document.getElementById("first_number").value ;
+   area=length*length;
+   perimeter=length*4;
+   document.getElementById("area_of_farm").innerHTML=area;
+document.getElementById("perimeter_of_farm").innerHTML=perimeter;
+
+
+}
+
